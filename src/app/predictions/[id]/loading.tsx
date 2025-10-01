01bc9513909da472767c244f3739b6757fcc7cf8
Marina2022 <marusiiiia@yandex.ru>
2025-10-01 13:20:37 +0200

    TS fix

--- src/app/predictions/[id]/loading.tsx
+++ src/app/predictions/[id]/loading.tsx
@@ -5,7 +5,7 @@ import TopBar from "@/components/predictionPage/1-top-bar/TopBar";
 const Loading = () => {
   return (
     <>
-      <TopBar/>
+      <TopBar prediction={null}/>
       <div className="pt-36 container pb-4 sm:pb-8 flex justify-center items-center min-h-[50vh]">
         <div className="text-center">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mx-auto mb-4"></div>
