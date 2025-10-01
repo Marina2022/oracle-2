8afb8290057da29b98af6f53836002068c64e55b
Marina2022 <marusiiiia@yandex.ru>
2025-10-01 14:21:02 +0200

    fixes

--- src/mocks/home-page/predictions.tsx
+++ src/mocks/home-page/predictions.tsx
@@ -1,27 +1,27 @@
 export const predictions = [
-  {
-    id: "bbb1a7a5b",
-    title: "Кто станет чемпионом РФПЛ в сезоне 2025-2026? Зенит или ЦСКА",
-    category: "Спорт",
-    content: "Чемпионат РФПЛ",
-    consensus: 75,
-    timeline: 1767225600,
-    participantsNumber: 1860,
-    growing: true,
-    models: [
-      {title: "ChatGPT-4o", precision: 76},
-      {title: "DeepSeek", precision: 70},
-      {title: "Grok", precision: 70},
-      {title: "Gemini", precision: 85}
-    ]
-  },
+  // {
+  //   id: "bbb1a7a5b",
+  //   title: "Кто станет чемпионом РФПЛ в сезоне 2025-2026? Зенит или ЦСКА",
+  //   category: "Спорт",
+  //   content: "Чемпионат РФПЛ",
+  //   consensus: 75,
+  //   timeline: 1767225600 + 86340,
+  //   participantsNumber: 1860,
+  //   growing: true,
+  //   models: [
+  //     {title: "ChatGPT-4o", precision: 76},
+  //     {title: "DeepSeek", precision: 70},
+  //     {title: "Grok", precision: 70},
+  //     {title: "Gemini", precision: 85}
+  //   ]
+  // },
   {
     id: "f6h8j0k2l",
     title: "Динамо М - Локомотив М 04.10.2025",
     category: "Спорт",
     content: "Матч РФПЛ",
     consensus: 65,
-    timeline: 1759536000,
+    timeline: 1759536000 + 86340,
     participantsNumber: 1247,
     growing: true,
     models: [
@@ -31,14 +31,13 @@ export const predictions = [
       {title: "Gemini", precision: 68}
     ]
   },
-
   {
     id: "g7i9k1m3n",
     title: "ЦСКА - Спартак 05.10.2025",
     category: "Спорт",
     content: "Матч РФПЛ",
     consensus: 56,
-    timeline: 1759622400,
+    timeline: 1759622400 + 86340,
     participantsNumber: 600,
     growing: false,
     models: [
@@ -48,14 +47,13 @@ export const predictions = [
       {title: "Gemini", precision: 72}
     ]
   },
-
   {
     id: "h8j0k2l4m",
     title: "Оренбург - Ростов 05.10.2025",
     category: "Спорт",
     content: "Матч РФПЛ",
     consensus: 59,
-    timeline: 1759622400,
+    timeline: 1759622400 + 86340,
     participantsNumber: 500,
     growing: true,
     models: [
@@ -65,14 +63,13 @@ export const predictions = [
       {title: "Gemini", precision: 65}
     ]
   },
-
   {
     id: "d4g6b8c0e",
     title: "Сочи — Пари НН 05.10.2025",
     category: "Спорт",
     content: "Матч РФПЛ",
     consensus: 57,
-    timeline: 1759622400,
+    timeline: 1759622400 + 86340,
     participantsNumber: 500,
     growing: true,
     models: [
@@ -82,14 +79,13 @@ export const predictions = [
       {title: "Gemini", precision: 67}
     ]
   },
-
   {
     id: "e5h7c9d1f",
     title: "Рубин - Крылья Советов 04.10.2025",
     category: "Спорт",
     content: "Матч РФПЛ",
     consensus: 60,
-    timeline: 1759622399,
+    timeline: 1759622399 + 86340,
     participantsNumber: 800,
     growing: true,
     models: [
@@ -99,77 +95,4 @@ export const predictions = [
       {title: "Gemini", precision: 70}
     ]
   },
-
-
-
-  // {
-  //   id: "f6i8d0e2g",
-  //   title: "Победитель Евровидения 2025",
-  //   category: "Музыка",
-  //   content: "Страна-победитель пока неизвестна",
-  //   consensus: 55,
-  //   timeline: "5 месяцев",
-  //   participantsNumber: 678,
-  //   growing: false,
-  //   models: [
-  //     {title: "ChatGPT", precision: 60},
-  //     {title: "YandexGPT", precision: 57},
-  //     {title: "Claude", precision: 54},
-  //     {title: "Gemini", precision: 56}
-  //   ]
-  // },
-  //
-  // {
-  //   id: "g7j9e1f3h",
-  //   title: "Количество заболевших гриппом зимой 2025",
-  //   category: "Здоровье",
-  //   content: "Прогноз: 120-150 тысяч человек",
-  //   consensus: 77,
-  //   timeline: "3 месяца",
-  //   participantsNumber: 945,
-  //   growing: false,
-  //   models: [
-  //     {title: "ChatGPT", precision: 78},
-  //     {title: "YandexGPT", precision: 75},
-  //     {title: "Claude", precision: 76},
-  //     {title: "Gemini", precision: 74}
-  //   ]
-  // },
-  //
-  // {
-  //   id: "h8k0f2g4i",
-  //   title: "Премия «Оскар» за лучший фильм 2025",
-  //   category: "Кино",
-  //   content: "Основные претенденты пока неизвестны",
-  //   consensus: 62,
-  //   timeline: "7 месяцев",
-  //   participantsNumber: 1123,
-  //   growing: true,
-  //   models: [
-  //     {title: "ChatGPT", precision: 64},
-  //     {title: "YandexGPT", precision: 61},
-  //     {title: "Claude", precision: 63},
-  //     {title: "Gemini", precision: 62}
-  //   ]
-  // },
-  //
-  // {
-  //   id: "i9l1g3h5j",
-  //   title: "Средняя цена на бензин в РФ в декабре 2025",
-  //   category: "Экономика",
-  //   content: "95-й бензин: 60-65 ₽/литр",
-  //   consensus: 84,
-  //   timeline: "10 месяцев",
-  //   participantsNumber: 1587,
-  //   growing: true,
-  //   models: [
-  //     {title: "ChatGPT", precision: 86},
-  //     {title: "YandexGPT", precision: 82},
-  //     {title: "Claude", precision: 83},
-  //     {title: "Gemini", precision: 85}
-  //   ]
-  // }
 ];
-
-
-
