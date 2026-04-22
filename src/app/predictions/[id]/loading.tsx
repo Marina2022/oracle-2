59f76d3dbef98752b8dad8486ac7c9b44e32111f
Marina2022 <marusiiiia@yandex.ru>
2026-04-22 14:15:39 +0300

    fix

---
+++ src/app/predictions/[id]/loading.tsx
@@ -0,0 +1,17 @@
+
+import TopBar from "@/components/pages/PredictionPage/1-top-bar/TopBar";
+
+const Loading = () => {
+  return (
+    <>
+      <TopBar prediction={null}/>
+      <div className="pt-36 container pb-4 sm:pb-8 flex justify-center items-center min-h-screen">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mx-auto mb-4"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
