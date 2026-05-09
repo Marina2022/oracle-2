72b23ebcf53cc046c5dad983ffae4c4ccf3fc784
Marina2022 <marusiiiia@yandex.ru>
2026-05-09 15:35:18 +0300

    fix1

--- src/features/prediction/actions/getMatches.ts
+++ src/features/prediction/actions/getMatches.ts
@@ -10,7 +10,7 @@ export const getMatches = async (filters = "") => {
     let url = `${process.env.API_URL}/matches`
     if (filters) url = url + "?" +  filters
 
-    console.log("url = ", url)
+    console.log("url === ", url)
 
     const res = await fetch(url);
 
